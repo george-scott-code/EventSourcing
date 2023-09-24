a120f734aff910988a5731fdee672fe26ef8e729
george-scott-code <george.scott.code@gmail.com>
2023-09-24 14:40:17 +0100

    document formatting

--- EventSourcing/Messages/LapCompleted.cs
+++ EventSourcing/Messages/LapCompleted.cs
@@ -3,29 +3,23 @@ using Confluent.Kafka;
 
 namespace EventSourcing.Messages;
 
-    public class LapCompleted : RaceEvent, ISerializer<LapCompleted>, IDeserializer<LapCompleted?>
+public class LapCompleted : RaceEvent, ISerializer<LapCompleted>, IDeserializer<LapCompleted?>
+{
+    public TimeSpan LapTime { get; init; }
+    public int LapNumber { get; init; }
+    public byte[] Serialize(LapCompleted data, SerializationContext context)
     {
-        public TimeSpan LapTime { get; init; }
-        public int LapNumber { get; init; }
-
-        public byte[] Serialize(LapCompleted data, SerializationContext context)
-        {
-            using var ms = new MemoryStream();
-
-            string jsonString = JsonSerializer.Serialize(data);
-            var writer = new StreamWriter(ms);
-
-            writer.Write(jsonString);
-            writer.Flush();
-            ms.Position = 0;
-
-            return ms.ToArray();
-        }
-
-        // TODO why no access modifier here?
-        LapCompleted? IDeserializer<LapCompleted?>.Deserialize(ReadOnlySpan<byte> data, bool isNull, SerializationContext context)
-        {
-            return JsonSerializer.Deserialize<LapCompleted>(data.ToArray());
-        }
+        using var ms = new MemoryStream();
+        string jsonString = JsonSerializer.Serialize(data);
+        var writer = new StreamWriter(ms);
+        writer.Write(jsonString);
+        writer.Flush();
+        ms.Position = 0;
+        return ms.ToArray();
     }
-
+    // TODO why no access modifier here?
+    LapCompleted? IDeserializer<LapCompleted?>.Deserialize(ReadOnlySpan<byte> data, bool isNull, SerializationContext context)
+    {
+        return JsonSerializer.Deserialize<LapCompleted>(data.ToArray());
+    }
+}
--- EventSourcing/Messages/RaceEvent.cs
+++ EventSourcing/Messages/RaceEvent.cs
@@ -1,9 +1,7 @@
 namespace EventSourcing.Messages;
 
-    public class RaceEvent
-    {
-        public Guid Id = Guid.NewGuid();
-        public int  CarNumber { get; init; }
-    }
-
-
+public class RaceEvent
+{
+    public Guid Id = Guid.NewGuid();
+    public int CarNumber { get; init; }
+}
