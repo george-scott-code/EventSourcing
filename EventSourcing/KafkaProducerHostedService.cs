b6b470ce3197ad2ea7a2cd25272eb5cb97631301
george-scott-code <george.scott.code@gmail.com>
2023-09-29 14:53:06 +0100

    add errror handler to producer

--- EventSourcing/KafkaProducerHostedService.cs
+++ EventSourcing/KafkaProducerHostedService.cs
@@ -23,6 +23,7 @@ internal partial class Program
             // TODO: support generic race events
             _producer = new ProducerBuilder<Null, LapCompleted>(config)
                 .SetValueSerializer(new LapCompletedSerializer())
+                .SetErrorHandler((_, error) => Console.Error.WriteLine(error))
                 .Build();
         }
 
